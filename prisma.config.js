85b02c670ab85788aca68e01a3451ecbebf431ba
wwwmisla <wwwmisla144@gmail.com>
2026-06-16 23:26:01 -0300

    feat: add suporte ao supabase

--- prisma.config.js
+++ prisma.config.js
@@ -4,7 +4,11 @@ import 'dotenv/config';
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL,
+      // URL principal (com pooler/pgbouncer para a API)
+      url: process.env.DATABASE_URL,
+
+      // URL direta (essencial para o Supabase aceitar os comandos do npx prisma migrate)
+      directUrl: process.env.DIRECT_URL,
   },
   generators: [
     {
